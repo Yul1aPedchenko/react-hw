dfa6098051680cd3e6b864cf6977bdfd41da2795
Yulia Pedchenko <pedcenkoulia2@gmail.com>
2025-11-07 16:57:36 +0200

    deleted hook that was defined but never use

--- src/components/hw/hw20/Hw20.jsx
+++ src/components/hw/hw20/Hw20.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useCallback } from "react";
 import { Searchbar } from "./hwComponents/Searchbar";
 import { ImageGallery } from "./hwComponents/ImageGallery";
 import { Button } from "./hwComponents/Button";
